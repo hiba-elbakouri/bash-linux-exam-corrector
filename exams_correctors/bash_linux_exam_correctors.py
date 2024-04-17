05f093d9e628a3abb6480d1bf13cfdffc13d3e33
Hiba Elbakouri <hiba.elbakouri@centrale-casablanca.ma>
2024-04-17 11:39:24 +0100

    clean code

--- exams_correctors/bash_linux_exam_correctors.py
+++ exams_correctors/bash_linux_exam_correctors.py
@@ -38,32 +38,11 @@ class BashLinuxExamCorrector(ExamCorrector):
      """
 
     _CRON_FILE = 'cron.txt'
-    _SALES_FILE = '../sales.txt'
+    _SALES_FILE = 'sales.txt'
     _SCRIPT_FILE = 'exam.sh'
 
     _FILES_TO_CORRECT = [_CRON_FILE, _SALES_FILE, _SCRIPT_FILE]
 
-
-class MongoDBExamCorrector(ExamCorrector):
-    """
-     A class to correct Linux exam files for candidates.
-
-     This class provides methods to fetch, extract, correct, and clean up exam files for candidates.
-     It includes functionality to handle cron, sales, and script files, and processes candidate files in a structured
-     manner.
-
-     Raises:
-         FileNotFoundError: If the cron, sales, or script file is not found during processing.
-
-     Returns:
-         None
-     """
-
-    _FILE_1 = 'hiba.txt'
-    _FILE_2 = 'simo.txt'
-    _FILE_2 = 'fo9ma.sh'
-
-    _FILES_TO_CORRECT = [_FILE_1, _FILE_2, _FILE_2]
 
 
 if __name__ == '__main__':
